ffbeadf4c8823b9f1a0ee9d08dabafe4ac19bf35
qedrohenripue <pedro.005henr@gmail.com>
2025-09-10 20:06:21 -0300

    mTLS setup

--- src/main/kotlin/com/example/demo/service/UserCrudService.kt
+++ src/main/kotlin/com/example/demo/service/UserCrudService.kt
@@ -5,9 +5,13 @@ import com.example.demo.hello.proto.HelloReply
 import com.example.demo.repository.UserRepository
 import com.example.demo.user.proto.CreateUserRequest
 import com.example.demo.user.proto.CreateUserResponse
+import com.example.demo.user.proto.DeleteUserRequest
+import com.example.demo.user.proto.DeleteUserResponse
 import com.example.demo.user.proto.UserCrudGrpc.UserCrudImplBase
+import io.grpc.Status
 import io.grpc.stub.StreamObserver
 import org.springframework.stereotype.Service
+import kotlin.jvm.optionals.getOrNull
 
 @Service
 class UserCrudService(
@@ -15,7 +19,11 @@ class UserCrudService(
 ): UserCrudImplBase() {
     override fun createUser(request: CreateUserRequest, responseObserver: StreamObserver<CreateUserResponse>) {
         if (request.username.isNullOrBlank() || request.password.isNullOrBlank()) {
-           // Throw RPC exception
+            responseObserver.onError(
+                Status.INVALID_ARGUMENT
+                    .withDescription("Username e password são obrigatórios.")
+                    .asRuntimeException()
+            )
             return
         }
 
@@ -24,6 +32,17 @@ class UserCrudService(
         )
 
         val response = CreateUserResponse.newBuilder().setMessage("Success.").build()
+        responseObserver.onNext(response)
+        responseObserver.onCompleted()
+    }
+
+    override fun deleteUser(request: DeleteUserRequest, responseObserver: StreamObserver<DeleteUserResponse>) {
+        val user = userRepository.findById(request.id).getOrNull() ?:
+            return responseObserver.onError(Status.NOT_FOUND.asRuntimeException())
+
+        userRepository.delete(user)
+
+        val response = DeleteUserResponse.newBuilder().setMessage("Success.").build()
         responseObserver.onNext(response)
         responseObserver.onCompleted()
     }
